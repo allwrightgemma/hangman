90b2bc226236fd921b10f395078458664137a75b
allwrightgemma <34656602+allwrightgemma@users.noreply.github.com>
2022-11-15 17:55:03 +0000

    created attempts span to keep track of letters guessed

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 const btnNewGame = document.getElementById("new-game");
 const baseUrl = "http://hangman-api.herokuapp.com/hangman";
 const btnGuess = document.getElementById("guess");
-let remainingGuesses = 7;
-let attempts = 0;
 
 async function startNewGame() {
   const response = await fetch(baseUrl, {
@@ -29,20 +27,30 @@ async function makeGuess() {
 
   let hangmanWord = document.querySelector(".hangman-word");
   let result = document.createTextNode(json.hangman);
+  let attemptsDisplay = document.querySelector(".attempts");
+  let attemptsSpan = document.createElement("span");
+  let attemptsContent = document.createTextNode(guessLetter);
+  let remainingDisplay = document.querySelector(".remaining");
+  let remainingGuesses = 7;
+
   if (json.correct === true) {
     hangmanWord.appendChild(result);
+    attemptsSpan.classList.add("correct");
+  } else {
+    attemptsSpan.classList.add("wrong");
   }
   token = json.token;
 
-  let remainingDisplay = document.querySelector(".remaining");
   remainingGuesses--;
   remainingDisplay.innerText = `${remainingGuesses}`;
 
-  let attemptsDisplay = document.querySelector(".attempts");
-  attempts++;
-
-  attemptsDisplay.innerText = `Attempts: ${attempts}`;
+  attemptsSpan.appendChild(attemptsContent);
+  attemptsDisplay.append(attemptsSpan);
 }
 
 btnNewGame.addEventListener("click", startNewGame);
 btnGuess.addEventListener("click", makeGuess);
+
+// want to update the hangman word with the letters
+// continue to add more than one correct or wrong class
+// after 7 guesses display the solution, hide guesses and display new game
